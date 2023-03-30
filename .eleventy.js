b2610e8c474f98e050e97ea8642e62e4c4827deb
Your Name <you@example.com>
2023-03-30 23:32:04 +0600

    JAM stack blog project

---
+++ .eleventy.js
@@ -0,0 +1,16 @@
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("./src/style.css");
+  eleventyConfig.addPassthroughCopy("./src/assets");
+
+  const { DateTime } = require("luxon");
+  eleventyConfig.addFilter("PostDate", (dateObj) => {
+    return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
+  });
+
+  return {
+    dir: {
+      input: "src",
+      output: "public",
+    },
+  };
+};
